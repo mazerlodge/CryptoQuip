d5cbe9e9846780625f9ef087cc2735865282dd68
mazerlodge <mazerlodge@hotmail.com>
2019-01-05 18:00:06 -0600

    Initial source and data file checkin w/ edits to alert style.

---
+++ CQ/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CQ
+//
+//  Created by Mazerlodge on 11/9/12.
+//  Copyright (c) 2012 Mazerlodge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
